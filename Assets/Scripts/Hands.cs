229945149857f08d334fc3b26ae0962302f64a19
unknown <nicholas.pace@live.com>
2015-04-18 15:28:51 +1000

    Base weapon functionality

---
+++ Assets/Scripts/Hands.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hands : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private float heldWeaponOffset = 10.0f;
+
+    private Weapon heldWeapon;
+
+    public Vector3 Position { get; set; }
+
+    public Weapon HeldWeapon
+    {
+        get
+        {
+            return this.heldWeapon;
+        }
+
+        set
+        {
+            if (this.heldWeapon != null)
+            {
+                Destroy(this.heldWeapon);
+            }
+
+            if (value == null)
+            {
+                return;
+            }
+
+            GameObject weaponGameObject = (GameObject)Instantiate(
+                value.transform.root.gameObject,
+                this.Position,
+                Quaternion.LookRotation(this.transform.forward, Vector3.up));
+
+            weaponGameObject.transform.parent = this.transform;
+
+            this.heldWeapon = weaponGameObject.GetComponentInChildren<Weapon>();
+        }
+    }
+
+	private void Update () {
+        if (this.heldWeapon != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.heldWeapon.StartUseWeapon();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                this.heldWeapon.EndUseWeapon();
+            }
+        }
+
+        this.Position = this.camera.ScreenToWorldPoint(Input.mousePosition);
+
+	    this.heldWeapon.transform.position = this.Position;
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private string weaponType = "BASE";
+
+    [SerializeField]
+    private Collider collider;
+
+    [SerializeField]
+    private Animation animation;
+
+    [SerializeField]
+    private AnimationClip animClip;
+
+    public string WeaponType
+    {
+        get
+        {
+            return this.weaponType;
+        }
+
+        set
+        {
+            this.weaponType = value;
+        }
+    }
+
+    public bool ColliderEnabled
+    {
+        get
+        {
+            if (this.collider != null)
+            {
+                return this.collider.enabled;
+            }
+
+            return false;
+        }
+
+        set
+        {
+            if (this.collider != null)
+            {
+                this.collider.enabled = value;
+            }
+        }
+    }
+
+    public virtual void StartUseWeapon()
+    {
+        this.ColliderEnabled = true;
+        this.StartPlayWeaponAnim();
+    }
+
+    public virtual void EndUseWeapon()
+    {
+        this.StopPlayWeaponAnim();
+        this.ColliderEnabled = false;
+    }
+
+    protected void StartPlayWeaponAnim()
+    {
+        if (this.animation != null && !this.animation.isPlaying)
+        {
+            this.animation.clip = this.animClip;
+            this.animation.Play();
+        }
+    }
+
+    protected void StopPlayWeaponAnim()
+    {
+        if (this.animation != null && this.animation.isPlaying)
+        {
+            this.animation.Stop();
+        }
+    }
+
+    private void Start()
+    {
+        this.ColliderEnabled = false;
+    }
+}
