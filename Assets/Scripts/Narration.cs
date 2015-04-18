c2ebe4ce8dc7b0d23bbfee47aed8d27e5fb3b715
Luke Bermingham <lukeh-b@hotmail.com>
2015-04-18 19:00:09 +1000

    narrator library with test scene

---
+++ Assets/Scripts/Narration.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Narration.cs" company="2BitStudios">
+//   2015
+// </copyright>
+// <summary>
+//   Refers to specific narrator lines
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+/// <summary>
+/// Refers to specific narrator lines
+/// </summary>
+public enum Narration
+{
+    Test1,
+    Test2
+}
