cdd34967ff58d5ebd4db6148f6393c8866a6d29b
Adam Rehn <adam.rehn@my.jcu.edu.au>
2015-04-19 16:27:22 +1000

    Updated the Candle Room scenario.

---
+++ Assets/Scripts/Scenarios/CandleRoom/TargetZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetZone : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		//If the table entered the target zone, set the flag accordingly
+		if (other.gameObject.tag == "Table") {
+			ScenarioManager.GetCurrentScenario().SetFlag(CandleRoomScenario.Flags.TableInPosition, true);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		//If the table left the target zone, set the flag accordingly
+		if (other.gameObject.tag == "Table") {
+			ScenarioManager.GetCurrentScenario().SetFlag(CandleRoomScenario.Flags.TableInPosition, false);
+		}
+	}
+}
